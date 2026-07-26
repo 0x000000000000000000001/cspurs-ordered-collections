4a575f7c7296ce4d0f50e1056ea9737b7f5886dc
0x1 <kevin.francart3@gmail.com>
2026-07-26 14:38:59 +0200

    feat: add C# FFI implementations

---
+++ src/Data/Map/Internal.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Data.Map.Internal;
+
+public static class FFI {
+    public static object NewBTree(object arg1) => throw new NotImplementedException("Not implemented: NewBTree");
+    public static object IsEmpty(object arg1) => throw new NotImplementedException("Not implemented: IsEmpty");
+    public static object InsertImpl(object arg1) => throw new NotImplementedException("Not implemented: InsertImpl");
+    public static object InsertWithImpl(object arg1) => throw new NotImplementedException("Not implemented: InsertWithImpl");
+    public static object LookupImpl(object arg1) => throw new NotImplementedException("Not implemented: LookupImpl");
+    public static object DeleteImpl(object arg1) => throw new NotImplementedException("Not implemented: DeleteImpl");
+    public static object KeysImpl(object arg1) => throw new NotImplementedException("Not implemented: KeysImpl");
+    public static object ValuesImpl(object arg1) => throw new NotImplementedException("Not implemented: ValuesImpl");
+    public static object UnionWithImpl(object arg1) => throw new NotImplementedException("Not implemented: UnionWithImpl");
+    public static object IntersectionWithImpl(object arg1) => throw new NotImplementedException("Not implemented: IntersectionWithImpl");
+    public static object DifferenceImpl(object arg1) => throw new NotImplementedException("Not implemented: DifferenceImpl");
+    public static object Size(object arg1) => throw new NotImplementedException("Not implemented: Size");
+}
